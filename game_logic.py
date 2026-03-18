95c26add607da6df921cc4af750c1d939aa5a15d
Thomas Luna <thomasluna991@gmail.com>
2026-03-17 21:11:39 -0500

    juego

---
+++ game_logic.py
@@ -0,0 +1,14 @@
+from secret_number import seed_secret_numbers, generate_secret_number
+from response import input_response
+seed = int(input("Enter a seed number:\n"))
+seed_secret_numbers(seed)
+secret = generate_secret_number()
+tries = 0
+correct = False
+while not correct:
+    guess = int(input("What is your guess:\n"))
+    tries += 1
+    
+    message, correct = input_response(secret, guess)
+    print(message)
+print(f"It took you {tries} tries!")
