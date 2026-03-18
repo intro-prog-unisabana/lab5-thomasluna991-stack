c0d248546f45e1741c8a0bfecbd6edf8e1d5b5ed
Thomas Luna <thomasluna991@gmail.com>
2026-03-17 21:21:31 -0500

    DEBUG

--- debug.py
+++ debug.py
@@ -1,17 +1,17 @@
-def find_max(a, b):
-  max_number = a
-  if a > b and a > c:
-    max_number = a
-  elif b > a and b > c:
-    max_number = b
+def find_max(a, b, c):
+  if a >= b and a >= c:
+      return a
+  elif b >= a and b >= c:
+      return b
   else:
-    max_number = c
+      return c
 
 #FREEZE CODE BEGIN
 x = int(input("Enter a number:\n"))
 y = int(input("Enter a number:\n"))
 z = int(input("Enter a number:\n"))
 #FREEZE CODE END
+maximum = find_max(x,y,z)
 
 print("Maximum value:", maximum)
 
