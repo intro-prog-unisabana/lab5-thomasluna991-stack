36b205511f65fd500ce83326941f89261e86dca0
Thomas Luna <thomasluna991@gmail.com>
2026-03-17 21:35:14 -0500

    mainn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import add, sub, multiply, divide, exponent, modulo, floor_divide, absolute
+import utils
 
 while True:
     op = input("Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit):\n").lower()
@@ -12,27 +12,27 @@ while True:
 
     if op == "absolute":
         num = float(input("Enter the number:\n"))
-        result = absolute(num)
+        result = utils.absolute(num)
     else:
         num1 = float(input("Enter the first number:\n"))
         num2 = float(input("Enter the second number:\n"))
 
         if op == "add":
-            result = add(num1, num2)
+            result = utils.add(num1, num2)
         elif op == "subtract":
-            result = sub(num1, num2)
+            result = utils.sub(num1, num2)
         elif op == "multiply":
-            result = multiply(num1, num2)
+            result = utils.multiply(num1, num2)
         elif op == "divide":
-            result = divide(num1, num2)
+            result = utils.divide(num1, num2)
         elif op == "exponent":
-            result = exponent(num1, num2)
+            result = utils.exponent(num1, num2)
         elif op == "modulo":
-            result = modulo(num1, num2)
+            result = utils.modulo(num1, num2)
         elif op == "floor_divide":
-            result = floor_divide(num1, num2)
+            result = utils.floor_divide(num1, num2)
 
     if isinstance(result, str):
         print(result)
     else:
-        print("The result is:", float(result))
+        print(f"The result is: {result}")
